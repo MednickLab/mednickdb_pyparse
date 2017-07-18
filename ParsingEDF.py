eb418e69b0858bffa96bb03edfa134d4c3f764fa
Jesse <jreye039@ucr.edu>
2017-07-18 14:17:12 -0700

    Parsing practice

---
+++ ParsingEDF.py
@@ -0,0 +1,24 @@
+import numpy
+import mne
+import json
+import sys
+import time
+
+EDF_file = mne.io.read_raw_edf(sys.argv[1])
+
+timeConvert = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(EDF_file.info["meas_date"]))
+
+edfDictionary = {}
+
+edfDictionary['bads'] = EDF_file.info["bads"]
+edfDictionary['ch_names'] = EDF_file.ch_names
+edfDictionary['highpass'] = EDF_file.info["highpass"]
+edfDictionary['lowpass'] = EDF_file.info["lowpass"]
+#Changed from epoch to date
+edfDictionary['meas_date'] = timeConvert
+edfDictionary['nchan'] = EDF_file.info["nchan"]
+edfDictionary['sfreq'] = EDF_file.info["sfreq"]
+edfDictionary['subject_info'] = EDF_file.info["subject_info"]
+edfDictionary['file_name'] = sys.argv[1]
+
+edfDictionaryToJSON = json.dumps(edfDictionary)
