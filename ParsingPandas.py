a09951165fb08f94a159797485f04f9441227369
sbang002 <sbang002@ucr.edu>
2017-07-26 12:14:22 -0700

    made Panda parsing more efficent

--- ParsingPandas.py
+++ ParsingPandas.py
@@ -1,39 +1,8 @@
 import pandas as pd
 import json
 
-
-
-#excel
-def ParsingExcel(path):
-	PandaFile = pd.read_excel(path)
-	FileKeys = PandaFile.keys()
-	#cycle through keys to see if we get hits on the five keys we want and keep track of which ones we hit
-	hit = []
-
-	count = PandaFile.shape
-	for _key in FileKeys:
-		if _key == "SubID":
-			hit.append("SubID")   
-		elif _key == "Study":
-			hit.append("Study")   
-		elif _key == "Visit":
-			hit.append("Visit")   
-		elif _key == "Session":
-			hit.append("Session")   
-		elif _key == "Task":
-			hit.append("Task")	
-	output_dict = []
-	for sub_data in PandaFile.iterrows():
-		print(json.loads(sub_data[1].to_json()))
-		output_dict.append(sub_data[1].to_json())
-	#print(output_dict[-1])
-	return output_dict
-
-	
-
-#CSV parsing returns jason object
-def ParsingCSV(path):
-	PandaFile = pd.read_csv(path)
+#parsing panda objects returns jason object
+def Parsing(PandaFile):
 	FileKeys = PandaFile.keys()
     #cycle through keys to see if we get hits on the five keys we want and keep track of which ones we hit
 	hit = []
@@ -53,11 +22,8 @@ def ParsingCSV(path):
 
 	output_dict = []
 	for sub_data in PandaFile.iterrows():
-		tempDict = {}
-		for _key in range(len(sub_data[1])):
-			tempDict[FileKeys[_key]] = sub_data[1][_key]
-		JsonObject = json.dumps(tempDict)
-		output_dict.append(JsonObject)
+		#print(json.loads(sub_data[1].to_json()))
+		output_dict.append(sub_data[1].to_json())
 	print(output_dict[-1])
 	return output_dict
 
@@ -69,8 +35,9 @@ def main(file):
 	List_of_JSON = []
 	temp = []
 	if file.endswith("xls") or file.endswith("xlsx"):
-		List_of_JSON = ParsingExcel(file)
+		temp = pd.read_excel(file)
 	else:
-		List_of_JSON = ParsingCSV(file)
+		temp = pd.read_csv(file)
+	List_of_JSON = Parsing(temp)
 	
-main("C:/source/mednickdb/temp/ERQ.xls")
+#main("C:/source/mednickdb/temp/ERQ.xls")#Encoding_Sub1_Visit1.csv")
