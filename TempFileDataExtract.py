fd5b2f070874ec5df3cf15be669bf04ef1542824
sbang002 <sbang002@ucr.edu>
2017-07-26 11:09:40 -0700

    Csv and Excel file Parsing Pandas

--- TempFileDataExtract.py
+++ TempFileDataExtract.py
@@ -3,9 +3,7 @@ import requests
 import json
 import os
 import ParsingEDF
-
-#enter path to dir containing the parsing files
-#exec("C:\\source\\Users\\User\\Desktop\\python-parsint\\ParsingEDF.py")
+import ParsingXLSX
  
 #directory to find all files in
 testdir = "C:\\source\\mednickdb\\temp"
@@ -18,16 +16,17 @@ serverdir = "http://127.0.0.1:8001/files/temp"
 def getAllFilesInTree(dirPath):
     _files = []
     for folder, subfolders, files in os.walk(dirPath):
-        print(files)
+        #print(files)
         for _file in files:
             filePath = os.path.join(os.path.abspath(folder), _file)
             _files.append(filePath)
-    print(_files)
+    #print(_files)
     return _files
 	
 def getAllTempFileRecords():
     response = requests.get("http://127.0.0.1:8001/files/temp/")
     records = response.json()
+    #print(records)
     _files = [i["path"] for i in records]
     return _files
 
@@ -37,19 +36,26 @@ def main():
 	filesInTemp = getAllFilesInTree(testdir)
 	filesInServerTemp = getAllTempFileRecords()
     #cycle through the files and if you find one that is a __ run script
-	
-	#print(filesInTemp)
+#	print(filesInServerTemp)
+#	print(filesInTemp)
 	
 	#goes through files in our temp folder and parse 
 	for _files in filesInTemp:
 	   if  _files.endswith(".edf"):
 	      ParsingEDF.main(_files) #run our python script
-		  
-	#goes through files in server temp folder and parse
-	for _files in filesInTemp:
-	   if  _files.endswith(".edf"):
-	      ParsingEDF.main(_files) #run our python script
-		  
+	   elif _files.endswith(".xls"):
+	      ParsingXLSX.main(_files)
+#		  		
+#	#goes through files in server temp folder and parse
+#	print("before For loop")
+#	print(filesInServerTemp)
+#	for _files in filesInServerTemp:
+#	   print("IN For Loop")
+#	   if  _files.endswith(".edf"):
+#	      ParsingEDF.main(_files) #run our python script
+#	   elif _files.endswith(".xlsx"):
+#	      temp = _files.split("\\")
+#	      ParsingXLSX.main(serverdir + temp[-1])  
            
     
 	#use for testing if call to ParsingEDF works
@@ -58,3 +64,4 @@ def main():
 
 #if __name__ == "__main__":
 main()
+	
