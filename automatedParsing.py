b0f0759cea2c7e1bce8b02b592e77cea71c7cf43
Se Hoon Bang <sbang002@ucr.edu>
2017-10-26 14:43:36 -0700

    parsing by file type not name

--- automatedParsing.py
+++ automatedParsing.py
@@ -5,61 +5,8 @@ import numpy
 import ParsingScoring as ps
 import ParsingEDF as pe
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Title: read_edf_annotations, resample_30s
-#Author: Stanislas Chambon
-#Date: 8/28/2017
-# Availability: https://github.com/mne-tools/mne-python/issues/4489
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def read_edf_annotations(fname, annotation_format="edf/edf+"):
-    """read_edf_annotations
-
-    Parameters:
-    -----------
-    fname : str
-        Path to file.
-
-    Returns:
-    --------
-    annot : DataFrame
-        The annotations
-    """
-    with open(fname, 'r', encoding='utf-8',
-              errors='ignore') as annotions_file:
-        tal_str = annotions_file.read()
-
-    if "edf" in annotation_format:
-        if annotation_format == "edf/edf+":
-            exp = '(?P<onset>[+\-]\d+(?:\.\d*)?)' + \
-                  '(?:\x15(?P<duration>\d+(?:\.\d*)?))?' + \
-                  '(\x14(?P<description>[^\x00]*))?' + '(?:\x14\x00)'
-
-        elif annotation_format == "edf++":
-            exp = '(?P<onset>[+\-]\d+.\d+)' + \
-                  '(?:(?:\x15(?P<duration>\d+.\d+)))' + \
-                  '(?:\x14\x00|\x14(?P<description>.*?)\x14\x00)'
-
-        annot = [m.groupdict() for m in re.finditer(exp, tal_str)]
-        good_annot = pd.DataFrame(annot)
-        good_annot = good_annot.query('description != ""').copy()
-        good_annot.loc[:, 'duration'] = good_annot['duration'].astype(float)
-        good_annot.loc[:, 'onset'] = good_annot['onset'].astype(float)
-    else:
-        raise ValueError('Not supported')
-
-    return good_annot
-
-
-def resample_30s(annot):
-    annot = annot.set_index('onset')
-    annot.index = pd.to_timedelta(annot.index, unit='s')
-    annot = annot.resample('30s').ffill()
-    annot = annot.reset_index()
-    annot['duration'] = 30.
-    return annot
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+#global variable
+SLEEPMAP = ""
 
 #File Types are:
 #- sleep (edf without scoring)
@@ -69,14 +16,26 @@ def resample_30s(annot):
 #- actigraphy (same as above)
 
 def sleep_parsing ( file ):
-    jsonobj = pe.EdfParse(file)
-    return jsonobj
+    try:
+        jsonobj = pe.EdfParse(file)
+    except:
+        return  None, 1, "Failed to open edf file" 
+    addedKey = []
+    for key in jsonobj.keys():
+        addedKey["edf_"+key] = jsonobj[key]
+    return addedKey, 0, ''
 
 
 
 def scoringfile_parsing ( file ) :
-    jsondict = ps.MakeJsonObj(file)                  
-    return jsondict
+    #try:
+    print(file)
+    jsondict = ps.MakeJsonObj(file) 
+    #except:
+    return  None, 1, "Failed to parse scorefile"
+    #line for change stages to stagemap stages
+    #jsondict = stagemapping(jsondict)
+    return jsondict, 0, ''
 
 #How is location of stagemap folder passed in?    
 def stagemapping (jsondict):
@@ -84,10 +43,10 @@ def stagemapping (jsondict):
     if 'studyid' in jsondict.keys:
         study = jsondict['studyid']
         #need to figure out how to know hwere stage map located
-        stagemapfiles = ps.getAllFilesInTree(_LOCATIONofSTAGEMAP_)
+        stagemapfiles = ps.getAllFilesInTree(SLEEPMAP)
         found = False
         smlocation = ''
-        while !found or study == '':
+        while (not found) or (study == ''):
             for i in Range(stagemapfiles):
                 if (study + '_') in stagemapfiles[i]:
                     found = True
@@ -103,8 +62,11 @@ def stagemapping (jsondict):
     
 def tabulardata_parsing ( file ):
     # works if only one page for tabular data
-    jsondict = ps.MakeJsonObj(file)    
-    return dict
+    try:
+        jsondict = ps.MakeJsonObj(file)
+    except:
+        return  None, 1, "Failed to parse tabular file"
+    return jsondict, 0, ''
 
 #def sleepdiaries_parsing ( file ):
 #CSV files
@@ -123,51 +85,70 @@ def tabulardata_parsing ( file ):
 #main function takes in a path to file
 #return as json string
 #if __name__ == '__main__': 
-def main(filetype, filepath, subject, visit= None, session = None, task = None) :
-    filepath = ''
-    if len(sys.argv) > 1:
-        filepath = sys.argv[1]  # FIXME using file and files as variable names is confusing, be more specific
-    else:
-        filepath = input("Enter absolute path to the head Directory containing the scorings folders: ")
-    
+def automated_parsing (filepath, filetype, subject = None, visit= None, session = None, task = None) :
+    #Delete when running program, Here just for testing
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #if len(sys.argv) > 1:
+    #    filepath = sys.argv[1]  # FIXME using file and files as variable names is confusing, be more specific
+    #else:
+    #    filepath = input("Enter absolute path to the head Directory containing the scorings folders: ")
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+        
+    error = 0
+    msg = ""
     jsonobj = []
     
     #choose correct file type
-    if 'scorefiles' not in filepath and '.edf' in filepath:
+    if filetype == "sleep":
         #call sleep parse function
-        jsonobj = sleep_parsing(filepath)
-    elif 'scorefiles' in filepath:
+        jsonobj, error , msg = sleep_parsing(filepath)
+    elif filetype =='scorefiles':
         #call scoring file parse function
-        jsonobj = scoringfile_parsing(filepath)
-    elif 'xlsx' in filepath: 
+        jsonobj, error , msg = scoringfile_parsing(filepath)
+    elif filetype == 'tabular': 
         #call tabulardata file parse function
-        jsonobj = tabulardata_parsing(filepath)
+        jsonobj, error , msg = tabulardata_parsing(filepath)
 #    elif sleepdiaries = filepath:
       #call sleepdiaries parse function
 #    elif actigraphy = filepath:
       #call actigraphy parse function
     
+    #check if error occured
+    if error == 1:
+        return jsonobj, error , msg
+    
+    
     if type(jsonobj) is dict:
         jsonobj['subject'] = subject
         if visit != None:
-            jsondict['visit'] = visit
+            jsonobj['visit'] = visit
         if session != None:
-            jsondict['session'] = session
+            jsonobj['session'] = session
         if task != None:
-            jsondict['task'] = task
-        jsondict = stagemapping(jsondict)
+            jsonobj['task'] = task
+        #jsonobj = stagemapping(jsonobj)
     #Is there stages to map for these files (none scorefiles) ??? (I dont think so)
     elif type (jsonobj) is list:
         for index in range(jsonobj):
             jsonobj[index]['subject'] = subject
             if visit != None:
-                jsondict[index]['visit'] = visit
+                jsonobj[index]['visit'] = visit
             if session != None:
-                jsondict[index]['session'] = session
+                jsonobj[index]['session'] = session
             if task != None:
-                jsondict[index]['task'] = task
+                jsonobj[index]['task'] = task
     
-    jsonlist = json.dumps(jsonobj)      
+    jsonlist = ''
+    if type (jsonobj) is list:
+        jsonlist = []
+        for index in range(len(jsonobj)):
+            jsonlist.append(json.dumps(jsonobj[index]))
+    else:
+        jsonlist = json.dumps(jsonobj)
    
     print (jsonobj)      #delete this line.
-    return jsonlist
+    return jsonlist, error, msg
+
+#for testing
+object, error, message = automated_parsing("D:/Users/Se Hoon Bang/Desktop/InternWork/DataFiles/subjectid10.edf", 'scorefiles')
+print (message)
