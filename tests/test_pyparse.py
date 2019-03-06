c03204b5ed1fe16e4399cf1ca84ff33481634f49
bdyetton@gmail.com <bdyetton@gmail.com>
2019-03-06 13:50:41 -0800

    fixign uild eerors 3

--- tests/test_pyparse.py
+++ tests/test_pyparse.py
@@ -2,6 +2,7 @@ import sys, os
 my_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append('./../mednickdb_pyparse/')
 sys.path.append('./mednickdb_pyparse/')
+sys.path.append('./../')
 from mednickdb_pyparse.mednickdb_auto_parse import automated_parsing
 import datetime
 """tests for python parsing part of mednickdb"""
